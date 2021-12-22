3521912fad1308e1837cd0538b06758784ff1406
AdamHooper <adamhooper3@gmail.com>
2021-12-22 15:59:10 +0000

    -- basic codewar complete

---
+++ betaMain.js
@@ -0,0 +1,25 @@
+/* 
+👉 Write your kata here!
+Make this a better story 
+
+Quiz results 
+
+if you get more than 5 out of 10 then youve passed a string for when theyve passed 
+
+if you get less than 5 out of 10 youve failed a string for when theyve failed 
+
+Ideas: 
+different quiz's have different number of answers so could we use percentages? 
+
+*/
+
+//👉 Write the function your CodeWarriors will start with below here: Make this more difficult 
+
+export function passingGrade(testScore) {
+    if ( testScore > 5){ 
+        const result = "congratulations"
+            return result;
+        } 
+        const result = "try again" 
+        return result;
+};
